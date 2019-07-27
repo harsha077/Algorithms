c19b213ea84965253ae77f6be040374ec1884321
Harsha vardhan <harshavardhanv77@gmail.com>
2019-07-27 10:36:54 -0400

    Uber question

---
+++ Algorithms/src/Arrays/UberATGPhoneScreen.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class UberATGPhoneScreen {
+	
+	/*[-5,-6,0,1,10,2,3]
+	[-5,-6,1,10,2,3]
+	[-5,0,10]
+	[-5,10]
+	[2,3]
+	[0,2,3]
+	[-6,0,1,10]
+	[-6,1,10]*/
+	
+	public static void main(String[] args) {
+		int[] array = {-5,3,2,-6,1,0,10};
+		int inp_sum = 5;
+	}
+	
+	public List getSumArrays(int[] array, int target) {
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		
+		return null;
+	}
+}
