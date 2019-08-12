243da0d47166887cd2ed3a269119e05407ef273b
harsha077 <harshavardhanv77@gmail.com>
2019-08-12 08:18:40 -0400

    new changes

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -13,67 +13,11 @@ public class Test {
 
 	
 	public static void main(String[] args) {
-		
-		sortTheMap();
-		//sortTheList();
-	}
-	
-	public static void sortTheMap(){
-		Map<String,Integer> map = new HashMap<String,Integer>();
-		List<String> resultList = new ArrayList<String>();
-		
-		map.put("user1", 3);
-		map.put("user2", 2);
-		map.put("user3", 2);
-		map.put("user4", 4);
-		
-		for(Entry<String,Integer> item:map.entrySet()){
-			//System.out.println(item.getKey() + " - "+item.getValue());
-		}
-		
-		System.out.println("=========================================");
-		
-		List<Entry<String,Integer>> sortedMap = new LinkedList<Entry<String,Integer>>(map.entrySet());
-		
-		Collections.sort(sortedMap, new Comparator<Entry<String, Integer>>(){
-
-			@Override
-			public int compare(Entry<String, Integer> o1,
-					Entry<String, Integer> o2) {
-				return o1.getValue().compareTo(o2.getValue());
-			}
-		});
-		
-		List<String> keyList = new ArrayList<String>(map.keySet());
-		
-		for(int i = map.size()-1;i>=0;i--){
-			String key = keyList.get(i);
-			System.out.println(key);
-		}
-		
-	}
-	
-	public static void sortTheList(){
 		List<Integer> list = new ArrayList<Integer>();
+		int[] arr = {1,2,3,4,5,6,7,8,9,0};
 		
-		list.add(110);
-		list.add(1);
-		list.add(10);
-		list.add(11);
 		
-		List<Integer> sortedList = new LinkedList<Integer>(list);
-		
-		Collections.sort(sortedList, new Comparator<Integer>(){
-
-			@Override
-			public int compare(Integer o1, Integer o2) {
-				return o1.compareTo(o2);
-			}
-		});
-		
-		for(Integer i: sortedList){
-			System.out.println(i);
-		}
-
 	}
+	
+	
 }
